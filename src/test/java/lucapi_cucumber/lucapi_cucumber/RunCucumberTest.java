68090491c2d200dc30ee459531e67b17534192c6
carlos-ruano <cdr.ruano@gmail.com>
2022-03-17 08:40:19 +0100

    cambio prueba

--- src/test/java/lucapi_cucumber/lucapi_cucumber/RunCucumberTest.java
+++ src/test/java/lucapi_cucumber/lucapi_cucumber/RunCucumberTest.java
@@ -11,4 +11,5 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @SelectClasspathResource("lucapi_cucumber/lucapi_cucumber")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class RunCucumberTest {
+	
 }
